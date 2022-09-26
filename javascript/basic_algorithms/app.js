5af987eab53a62a991b3026e29d520212cc826dd
bearom95 <bearom95@gmail.com>
2022-09-26 18:55:30 +0200

    iteraciones 2 y 3

--- javascript/basic_algorithms/app.js
+++ javascript/basic_algorithms/app.js
@@ -4,3 +4,50 @@ let x = 50;
 let h = 5;
 let y = 10;
 let z = h + y;
+
+
+// Iteración 2 Variables Avanzadas
+const character = {name: 'Jack Sparrow', age: 10};
+character.age = 25;
+ 
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.")
+
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log(toy1.price + toy2.price);
+
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}; 
+
+globalBasePrice = 25000;
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
+console.log(car1.finalPrice);
+console.log(car2.finalPrice);
+
+
+// Iteración 3 Operadores
+console.log(10*5);
+console.log(10/2);
+console.log(15%9);
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
+let c = 10;
+let m = 5;
+let i = c * m;
+console.log(i);
+
+
+// Iteración 4 Arrays
+
+
