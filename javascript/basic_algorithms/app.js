0988d491e4513c61fbafdb9699d3d37f9777720d
bearom95 <bearom95@gmail.com>
2022-09-26 19:31:32 +0200

    Iteracion 4

--- javascript/basic_algorithms/app.js
+++ javascript/basic_algorithms/app.js
@@ -49,5 +49,30 @@ console.log(i);
 
 
 // Iteración 4 Arrays
+const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+console.log(avengers[0]);
+
+avengers[0]= "IRONMAN";
+console.log(avengers);
+
+console.log(avengers.length);
 
 
+const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+rickAndMortyCharacters.push("Morty", "Summer");
+console.log(rickAndMortyCharacters);    // Imprimo para comprobar que está bien el nuevo array
+console.log(rickAndMortyCharacters[rickAndMortyCharacters.length-1]);
+
+
+const rickAndMortyCharacters2 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters2.pop();
+console.log(rickAndMortyCharacters2[0]);
+console.log(rickAndMortyCharacters2[rickAndMortyCharacters.length-1]);
+
+const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+rickAndMortyCharacters3.splice(1, 1);
+console.log(rickAndMortyCharacters3);
+// Para este último ejercicio hemos usado .splice()
+// .splice sirve para borrar algun elemento del array y poner otro en su lugar
+// tiene 3 parametros .splice(index donde inicias el cambio, nº de elementos que eliminas, el valor por el que sustituyes el eliminado)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice 
