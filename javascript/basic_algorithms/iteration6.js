4b55595d2da7112b352317b03ee05278ff158d0c
bearom95 <bearom95@gmail.com>
2022-09-27 18:09:36 +0200

    Ejercicio 6 terminado

--- javascript/basic_algorithms/iteration6.js
+++ javascript/basic_algorithms/iteration6.js
@@ -3,3 +3,24 @@
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+//Sacar solo los números pares
+for (let i = 0; i < 10; i++) {
+    if (i%2 == 0){
+    console.log(i);
+    }
+}
+
+/*1.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle empieza en 0 y termina en 10. 
+Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
+y cambia el mensaje en la última vuelta a 'Dormido!'.*/
+
+for (let i = 0; i < 11; i++){
+    if (i < 10){
+        console.log(i + " Intentando dormir");
+    } else if (i == 10){
+        console.log(i + " Dormido!");
+    }
+   
+}
