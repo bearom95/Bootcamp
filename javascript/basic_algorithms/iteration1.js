d8aa022a0a5f0cecd11447387ee12946f1816c87
bearom95 <bearom95@gmail.com>
2022-09-27 17:10:27 +0200

    Mejora de ejercicios y separacion en diferentes archivos

---
+++ javascript/basic_algorithms/iteration1.js
@@ -0,0 +1,8 @@
+// Iteración 1 Variables
+// Declarar variables
+
+let myFavoriteHero = "Hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
--- javascript/basic_algorithms/iteration4.js
+++ javascript/basic_algorithms/iteration4.js
@@ -1,53 +1,3 @@
-// Iteración 1 Variables
-let myFavoriteHero = "Hulk";
-let x = 50;
-let h = 5;
-let y = 10;
-let z = h + y;
-
-
-// Iteración 2 Variables Avanzadas
-const character = {name: 'Jack Sparrow', age: 10};
-character.age = 25;
- 
-
-let firstName = 'Jon'; 
-let lastName = 'Snow'; 
-let age = 24; 
-console.log("Soy " + firstName + " " + lastName + ", tengo " + age + " años y me gustan los lobos.")
-
-
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
-console.log(toy1.price + toy2.price);
-
-
-let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}; 
-
-globalBasePrice = 25000;
-car1.finalPrice = globalBasePrice + car1.basePrice;
-car2.finalPrice = globalBasePrice + car2.basePrice;
-console.log(car1.finalPrice);
-console.log(car2.finalPrice);
-
-
-// Iteración 3 Operadores
-console.log(10*5);
-console.log(10/2);
-console.log(15%9);
-let p = 10;
-let j = 5;
-let o = p + j;
-console.log(o);
-
-let c = 10;
-let m = 5;
-let i = c * m;
-console.log(i);
-
-
 // Iteración 4 Arrays
 const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
 console.log(avengers[0]);
@@ -72,6 +22,7 @@ console.log(rickAndMortyCharacters2[rickAndMortyCharacters.length-1]);
 const rickAndMortyCharacters3 = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 rickAndMortyCharacters3.splice(1, 1);
 console.log(rickAndMortyCharacters3);
+
 // Para este último ejercicio hemos usado .splice()
 // .splice sirve para borrar algun elemento del array y poner otro en su lugar
 // tiene 3 parametros .splice(index donde inicias el cambio, nº de elementos que eliminas, el valor por el que sustituyes el eliminado)
