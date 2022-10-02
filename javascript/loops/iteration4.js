24a9613eb3ad0057cac81a3f5a2e0b862892c3fe
bearom95 <bearom95@gmail.com>
2022-10-02 21:50:41 +0200

    Ejercicio 4 Loops

---
+++ javascript/loops/iteration4.js
@@ -0,0 +1,14 @@
+// Iteracion 4 For ... in
+
+// Usa un for...in para imprimir por consola los datos del alienígena.. Puedes usar este objeto:
+
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+}
+
+for (let info in alien) {
+    console.log("Alien tiene la clave " + info + " con el valor: " + alien[info]);
+}
