aaf09520fd1d45c5922e2ab419a4f2037c4334ce
bearom95 <bearom95@gmail.com>
2022-10-02 22:51:08 +0200

    Ejercicio 1 Review

---
+++ javascript/basics_review/iteration1.js
@@ -0,0 +1,21 @@
+// Iteracion 1 Mix FOR e includes
+
+/* Dado el siguiente javascript usa forof para recorrer el array de películas, genera un nuevo array con las categorías de las películas e imprime por consola el array de categorías. Ten en cuenta que las categorías no deberían repetirse. Para filtrar las categorías puedes ayudarte de la función .includes() */
+
+const movies = [
+    {title: 'Madaraspar', duration: 192, categories: ['comedia', 'aventura']},
+    {title: 'Spiderpan', duration: 122, categories: ['aventura', 'acción']},
+    {title: 'Solo en Whatsapp', duration: 223, categories: ['comedia', 'thriller']},
+    {title: 'El gato con guantes', duration: 111, categories: ['comedia', 'aventura', 'animación']},
+]
+
+let categoriasTotales = [];
+for (let movie of movies) {
+    for (let category of movie.categories) {         //meterse de nuevo en cada elemento del array categories
+        if (categoriasTotales.includes(category) == false) {
+            categoriasTotales.push(category)
+        }
+    }
+}
+
+console.log(categoriasTotales);
