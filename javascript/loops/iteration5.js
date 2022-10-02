ef0b0dd183d732cc1bf172dd7d0ec3203df026f5
bearom95 <bearom95@gmail.com>
2022-10-02 22:10:15 +0200

    Ejercicio 5 Loops

---
+++ javascript/loops/iteration5.js
@@ -0,0 +1,14 @@
+// Iteracion 5 Usando FOR
+
+// Usa un bucle for para recorrer todos los destinos del array y elimina los 
+// elementos que tengan el id 11 y 40. Imprime en un console log el array. Puedes usar este array:
+
+const placesToTravel = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'}, 
+                        {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
+
+for (let destino of placesToTravel) {
+   if (destino.id !== 11 && destino.id !== 40) {
+        console.log(destino.name);
+    } 
+}
+
